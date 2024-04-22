4a81e2f69688b450d8bff9448acab97452393d2b
carlosJr <carlosjraurgusto3@gmail.com>
2024-04-22 16:44:10 -0300

    Adicionar/modificar arquivos do projeto Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from'@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [ AppComponent ],
+  imports: [ BrowserModule, AppRoutingModule, BrowserAnimationsModule, ReactiveFormsModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
