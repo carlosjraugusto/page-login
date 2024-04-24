65cc5d86157f2ce50b0f827f26c5c3403e977e21
carlosJr <carlosjraurgusto3@gmail.com>
2024-04-24 09:38:28 -0300

    continua

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from'@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ModuleComponent } from './module/module.component';
+import { HomeComponent } from './module/home/home.component';
 
 @NgModule({
-  declarations: [ AppComponent ],
-  imports: [ BrowserModule, AppRoutingModule, BrowserAnimationsModule, ReactiveFormsModule,HttpClientModule],
+  declarations: [AppComponent, ModuleComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    //primeNG
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
