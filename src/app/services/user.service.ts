f1c37ee4dfb964fd98339092ec097f758d221648
carlosJr <carlosjraurgusto3@gmail.com>
2024-04-29 07:42:36 -0300

    erros

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environment/environment';
+import { SignupUserRequest } from '../module/users/signupUserRequest';
+import { SignupUserResponse } from '../module/users/signupUserResponse';
+import { AuthRequest } from '../module/users/auth/AuthRequeste';
+import { AuthResponse } from '../module/users/auth/AuthResponse';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) {}
+
+  signupUser(requestData: SignupUserRequest): Observable<SignupUserResponse> {
+    return this.http.post<SignupUserResponse>(
+      `${this.API_URL}/user`,
+      requestData,
+    );
+  }
+
+  authUser(requestData: AuthRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestData);
+  }
+}
